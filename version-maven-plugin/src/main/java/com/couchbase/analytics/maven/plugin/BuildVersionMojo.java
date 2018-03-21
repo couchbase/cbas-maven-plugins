ac71c28e4e175d341ec241be581cbceb7858156c
Michael Blow <michael.blow@couchbase.com>
2018-03-20 20:11:07 -0400

    MB-28191, MB-28650: version fixes
    - use repo forall to determine SHAs for dev builds
    - look for manifest properly for official builds
    
    Change-Id: If07c983e1b0b3fec22dfe93640fc9433877a788a
    Reviewed-on: http://review.couchbase.org/91292
    Reviewed-by: Build Bot <build@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Murtadha Hubail <Murtadha.hubail@couchbase.com>

--- version-maven-plugin/src/main/java/com/couchbase/analytics/maven/plugin/BuildVersionMojo.java
+++ version-maven-plugin/src/main/java/com/couchbase/analytics/maven/plugin/BuildVersionMojo.java
@@ -5,38 +5,42 @@ package com.couchbase.analytics.maven.plugin;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
-import java.util.HashSet;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
 
 import javax.xml.bind.JAXBContext;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.hyracks.util.JSONUtil;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.json.JSONObject;
 
 import com.couchbase.analytics.maven.plugin.jaxb.Annotation;
 import com.couchbase.analytics.maven.plugin.jaxb.Manifest;
 import com.couchbase.analytics.maven.plugin.jaxb.Project;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Mojo(name = "generate-version-info")
 public class BuildVersionMojo extends AbstractMojo {
 
     private static final String MANIFEST_FILE_NAME = "manifest.xml";
-    private static final String REPO_DIR_NAME = ".repo";
     private static final String BUILD_NUMBER_FIELD = "build.number";
 
     @Parameter(required = true)
@@ -45,27 +49,33 @@ public class BuildVersionMojo extends AbstractMojo {
     @Parameter()
     File inputFile;
 
+    @Parameter()
+    File baseDir;
+
     @Parameter(required = true)
     File outputFile;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
-            JSONObject jsonObject = new JSONObject().put("build.date", new Date()).put("build.version", productVersion);
-            inputFile = getManifestFile();
+            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectNode jsonObject = objectMapper.createObjectNode();
+            jsonObject.putPOJO("build.date", String.valueOf(new Date())).put("build.version", productVersion);
+            if (inputFile == null) {
+                inputFile = getManifestFile();
+            }
             final String missingBuildNumberMsg = "<local build on " + InetAddress.getLocalHost().getHostName() + ">";
             if (inputFile != null) {
                 getLog().info("Populating build info from manifest at: " + inputFile.getAbsolutePath());
                 JAXBContext ctx = JAXBContext.newInstance(Manifest.class);
                 Manifest manifest = (Manifest) ctx.createUnmarshaller().unmarshal(inputFile);
-                Set<JSONObject> projectJSONs = new HashSet<>();
+                ArrayNode projectJSONs = jsonObject.putArray("projects");
                 for (Project project : manifest.getProject()) {
-                    JSONObject projectJSON =
-                            new JSONObject().put("name", project.getName()).put("revision", project.getRevision());
+                    ObjectNode projectJSON = projectJSONs.addObject().put("name", project.getName()).put("revision",
+                            project.getRevision());
                     if (project.getUpstream() != null) {
                         projectJSON.put("upstream", project.getUpstream());
                     }
-                    projectJSONs.add(projectJSON);
                     if ("build".equals(project.getName())) {
                         final Optional<Annotation> buildNumAnno = project.getAnnotation().stream()
                                 .filter(annotation -> "BLD_NUM".equals(annotation.getName())).findFirst();
@@ -76,21 +86,48 @@ public class BuildVersionMojo extends AbstractMojo {
                         }
                     }
                 }
-                jsonObject.put("projects", projectJSONs);
             } else {
                 jsonObject.put(BUILD_NUMBER_FIELD, missingBuildNumberMsg);
+                ArrayNode projects = jsonObject.putArray("projects");
+                if (!SystemUtils.IS_OS_WINDOWS) {
+                    Process process = new ProcessBuilder("repo", "forall", "-r", ".*", "-c",
+                            "echo $REPO_PROJECT:$REPO_LREV:$REPO_RREV").redirectErrorStream(true).start();
+                    process.waitFor();
+                    IOUtils.readLines(process.getInputStream(), StandardCharsets.UTF_8).stream()
+                            .map(line -> StringUtils.split(line, ":"))
+                            .forEach(pair -> projects.addPOJO(new ProjectRevision(pair[0], pair[1], pair[2])));
+                }
             }
             outputFile.getParentFile().mkdirs();
             try (Writer outWriter = new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8)) {
-                outWriter.write(jsonObject.toString(4));
+                outWriter.write(JSONUtil.convertNode(jsonObject));
             }
         } catch (Exception e) {
             throw new MojoFailureException("error", e);
         }
     }
 
-    private static File getManifestFile() throws IOException {
-        Path currentPath = Paths.get("").toAbsolutePath();
+    @JsonInclude(Include.NON_NULL)
+    private static class ProjectRevision {
+        @JsonProperty
+        String name;
+
+        @JsonProperty
+        String revision;
+
+        @JsonProperty
+        String upstream;
+
+        ProjectRevision(String name, String revision, String manifestRevision) {
+            this.name = name;
+            this.revision = revision;
+            this.upstream = Objects.equals(revision, manifestRevision) ? null : manifestRevision;
+        }
+    }
+
+    private File getManifestFile() {
+        Path currentPath = baseDir != null ? baseDir.toPath() : Paths.get(".").toAbsolutePath();
+        getLog().info("Looking for manifest file starting here: " + currentPath);
         File manifestFile = null;
         while (manifestFile == null) {
             manifestFile = findManifestFile(currentPath);
@@ -99,21 +136,12 @@ public class BuildVersionMojo extends AbstractMojo {
                 break;
             }
         }
+        getLog().info("Determined manifest file to be: " + manifestFile);
         return manifestFile;
     }
 
-    private static File findManifestFile(Path dir) throws IOException {
-        try (DirectoryStream<Path> dirFiles = Files.newDirectoryStream(dir.toAbsolutePath())) {
-            for (Path file : dirFiles) {
-                if (file.endsWith(MANIFEST_FILE_NAME)) {
-                    return file.toFile();
-                }
-                if (file.toFile().isDirectory() && file.endsWith(REPO_DIR_NAME)) {
-                    // found .repo dir --> should find manifest inside it
-                    return findManifestFile(file);
-                }
-            }
-        }
-        return null;
+    private static File findManifestFile(Path dir) {
+        File candidate = new File(dir.toFile(), MANIFEST_FILE_NAME);
+        return candidate.exists() ? candidate : null;
     }
 }
