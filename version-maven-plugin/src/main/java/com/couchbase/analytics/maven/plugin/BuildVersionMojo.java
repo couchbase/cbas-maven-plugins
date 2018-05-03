22963ce052e100a6b98f4c611d4d7b7e0c9987b9
Michael Blow <michael.blow@couchbase.com>
2018-05-03 13:41:17 -0400

    MB-29503: use repo manifest -r for version information
    The solution (repo forall-based) to determine version information
    from repo state is not working on the version of repo available on
    Windows.  Rework the solution to instead use repo manifest -r,
    which is working on all known platforms.
    
    Change-Id: Idbeaf181c07b84f035990a7dbba14f9965f362d4
    Reviewed-on: http://review.couchbase.org/93635
    Reviewed-by: Murtadha Hubail <Murtadha.hubail@couchbase.com>
    Tested-by: Michael Blow <michael.blow@couchbase.com>

--- version-maven-plugin/src/main/java/com/couchbase/analytics/maven/plugin/BuildVersionMojo.java
+++ version-maven-plugin/src/main/java/com/couchbase/analytics/maven/plugin/BuildVersionMojo.java
@@ -6,18 +6,13 @@ package com.couchbase.analytics.maven.plugin;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Optional;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
@@ -33,50 +28,41 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public abstract class BuildVersionMojo extends AbstractMojo {
 
-    private static final String MANIFEST_FILE_NAME = "manifest.xml";
     private static final String BUILD_NUMBER_FIELD = "build.number";
 
     @Parameter()
     File inputFile;
 
-    @Parameter()
-    File baseDir;
+    @Parameter(defaultValue = "${project.build.directory}")
+    private File projectBuildDir;
 
     protected ObjectNode getBuildVersionJson() throws InterruptedException, IOException, JAXBException {
         ObjectMapper objectMapper = new ObjectMapper();
         ObjectNode jsonObject = objectMapper.createObjectNode();
-        if (inputFile == null) {
-            inputFile = getManifestFile();
-        }
+        Unmarshaller unmarshaller = JAXBContext.newInstance(Manifest.class).createUnmarshaller();
         final String missingBuildNumberMsg = "<local build on " + InetAddress.getLocalHost().getHostName() + ">";
+        Manifest manifest;
         if (inputFile != null) {
             getLog().info("Populating build info from manifest at: " + inputFile.getAbsolutePath());
-            JAXBContext ctx = JAXBContext.newInstance(Manifest.class);
-            Manifest manifest = (Manifest) ctx.createUnmarshaller().unmarshal(inputFile);
-            ArrayNode projectJSONs = jsonObject.putArray("projects");
-            for (Project project : manifest.getProject()) {
-                projectJSONs
-                        .addPOJO(new ProjectRevision(project.getName(), project.getRevision(), project.getUpstream()));
-                if ("build".equals(project.getName())) {
-                    final Optional<Annotation> buildNumAnno = project.getAnnotation().stream()
-                            .filter(annotation -> "BLD_NUM".equals(annotation.getName())).findFirst();
-                    if (buildNumAnno.isPresent()) {
-                        jsonObject.put(BUILD_NUMBER_FIELD, buildNumAnno.get().getValue());
-                    } else {
-                        jsonObject.put(BUILD_NUMBER_FIELD, missingBuildNumberMsg);
-                    }
-                }
-            }
         } else {
-            jsonObject.put(BUILD_NUMBER_FIELD, missingBuildNumberMsg);
-            ArrayNode projects = jsonObject.putArray("projects");
-            if (!SystemUtils.IS_OS_WINDOWS) {
-                Process process = new ProcessBuilder("repo", "forall", "-r", ".*", "-c",
-                        "echo $REPO_PROJECT:$REPO_LREV:$REPO_RREV").start();
-                process.waitFor();
-                IOUtils.readLines(process.getInputStream(), StandardCharsets.UTF_8).stream()
-                        .map(line -> StringUtils.split(line, ":"))
-                        .forEach(pair -> projects.addPOJO(new ProjectRevision(pair[0], pair[1], pair[2])));
+            getLog().info("Populating build info from repo manifest -r output");
+            inputFile = File.createTempFile("manifest", ".xml", projectBuildDir);
+            Process process = new ProcessBuilder("repo", "manifest", "-r")
+                    .redirectError(ProcessBuilder.Redirect.INHERIT).redirectOutput(inputFile).start();
+            process.waitFor();
+        }
+        manifest = (Manifest) unmarshaller.unmarshal(inputFile);
+        ArrayNode projectJSONs = jsonObject.putArray("projects");
+        for (Project project : manifest.getProject()) {
+            projectJSONs.addPOJO(new ProjectRevision(project.getName(), project.getRevision(), project.getUpstream()));
+            if ("build".equals(project.getName())) {
+                final Optional<Annotation> buildNumAnno = project.getAnnotation().stream()
+                        .filter(annotation -> "BLD_NUM".equals(annotation.getName())).findFirst();
+                if (buildNumAnno.isPresent()) {
+                    jsonObject.put(BUILD_NUMBER_FIELD, buildNumAnno.get().getValue());
+                } else {
+                    jsonObject.put(BUILD_NUMBER_FIELD, missingBuildNumberMsg);
+                }
             }
         }
         return jsonObject;
@@ -112,27 +98,4 @@ public abstract class BuildVersionMojo extends AbstractMojo {
         }
     }
 
-    protected File getManifestFile() {
-        Path currentPath = baseDir != null ? baseDir.toPath() : Paths.get(".").toAbsolutePath();
-        getLog().info("Looking for manifest file starting here: " + currentPath);
-        File manifestFile = null;
-        while (manifestFile == null) {
-            manifestFile = findManifestFile(currentPath);
-            currentPath = currentPath.getParent();
-            if (currentPath == null) {
-                break;
-            }
-        }
-        if (manifestFile != null) {
-            getLog().info("Determined manifest file to be: " + manifestFile);
-        } else {
-            getLog().info("Unable to find manifest file; falling back to repo state");
-        }
-        return manifestFile;
-    }
-
-    protected static File findManifestFile(Path dir) {
-        File candidate = new File(dir.toFile(), MANIFEST_FILE_NAME);
-        return candidate.exists() ? candidate : null;
-    }
 }
--- version-maven-plugin/src/main/java/com/couchbase/analytics/maven/plugin/GenerateBuildVersionPropertiesMojo.java
+++ version-maven-plugin/src/main/java/com/couchbase/analytics/maven/plugin/GenerateBuildVersionPropertiesMojo.java
@@ -7,33 +7,17 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Date;
-import java.util.Objects;
-import java.util.Optional;
 
-import javax.xml.bind.JAXBContext;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
-import org.apache.hyracks.util.JSONUtil;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import com.couchbase.analytics.maven.plugin.jaxb.Annotation;
-import com.couchbase.analytics.maven.plugin.jaxb.Manifest;
-import com.couchbase.analytics.maven.plugin.jaxb.Project;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Mojo(name = "generate-version-info")
@@ -52,7 +36,11 @@ public class GenerateBuildVersionPropertiesMojo extends BuildVersionMojo {
             jsonObject.putPOJO("build.date", String.valueOf(new Date())).put("build.version", productVersion);
             outputFile.getParentFile().mkdirs();
             try (Writer outWriter = new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8)) {
-                outWriter.write(JSONUtil.convertNode(jsonObject));
+                final ObjectMapper mapper =
+                        new ObjectMapper().configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true)
+                                .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, true);
+                outWriter.write(mapper.writerWithDefaultPrettyPrinter()
+                        .writeValueAsString(mapper.treeToValue(jsonObject, Object.class)));
             }
         } catch (Exception e) {
             getLog().warn("Ignoring unexpected exception: " + e, e);
